dc8332e382f7afaa02ce14ada7820885fb9803dd
Matloob Hussain <devmatloob1224@gmail.com>
2024-05-02 01:11:44 +0500

    1-Sheet Issue Fixed(Color Chooses Issue pending) 2-Edit Item Page 3-Setting Page 4-New Group Dialog 5-Import From CVS Dialog

--- lib/modules/settings/pages/settings_page.dart
+++ lib/modules/settings/pages/settings_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:xstock/modules/settings/dialogs/import_from_cvs_dialog.dart';
 import 'package:xstock/modules/settings/dialogs/switch_account_dialog.dart';
 import 'package:xstock/modules/settings/models/account_title_model.dart';
 import 'package:xstock/modules/settings/widgets/account_tile.dart';
 import 'package:xstock/modules/settings/widgets/phone_varification_widget.dart';
 import 'package:xstock/modules/settings/widgets/settings_tile.dart';
+import 'package:xstock/modules/settings/widgets/slide_able_action.dart';
 import 'package:xstock/ui/widgets/appbar_widget.dart';
 import 'package:xstock/ui/widgets/on_click.dart';
 
@@ -26,9 +28,34 @@ class SettingsPage extends StatelessWidget {
               SizedBox(
                 height: 18,
               ),
-              PhoneVerificationWidget(title: 'Phone Vibration', onTap: () {  },),
-              AccountTile(model: AccountTitleModel(name: "jone deper", email: "jone@deper.one", id: 1),),
-              SizedBox(height: 10,),
+              PhoneVerificationWidget(
+                title: 'Phone Vibration',
+                onTap: () {},
+              ),
+              Slidable(
+                closeOnScroll: false,
+                startActionPane: ActionPane(
+                  motion: const BehindMotion(),
+                  children: [
+                    SlidableActionTest(
+                      padding: EdgeInsets.zero,
+                      backgroundColor: Colors.red,
+                      borderRadius: BorderRadius.circular(16),
+                      spacing: 12,
+                      foregroundColor: Colors.white,
+                      icon: 'assets/images/svg/ic_delete.svg',
+                      onPressed: (BuildContext context) {},
+                    )
+                  ],
+                ),
+                child: AccountTile(
+                  model: AccountTitleModel(
+                      name: "jone deper", email: "jone@deper.one", id: 1),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
               OnClick(
                   onTap: () {
                     showDialog(
